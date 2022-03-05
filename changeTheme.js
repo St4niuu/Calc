aca37b3e6ad511256576c73e159ee453345d0f33
St4niuu <96002040+St4niuu@users.noreply.github.com>
2022-03-05 17:11:07 +0100

    Update changeTheme.js

--- changeTheme.js
+++ changeTheme.js
@@ -27,7 +27,7 @@ function changeTheme()
     document.querySelector(".trigger").classList.remove(classes[1], classes[2]);
     state = 0;
     document.getElementsByTagName("link")[2].remove();
-    document.getElementsByTagName("link")[3].remove();
+    document.getElementsByTagName("link")[2].remove();
   }
 }
 document.querySelector(".trigger").addEventListener("click", changeTheme);
