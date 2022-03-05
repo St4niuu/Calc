98956188fca3c7ab48e6c5709c034024d21a8307
St4niuu <96002040+St4niuu@users.noreply.github.com>
2022-03-05 17:19:45 +0100

    Update changeTheme.js

--- changeTheme.js
+++ changeTheme.js
@@ -26,8 +26,7 @@ function changeTheme()
   {
     document.querySelector(".trigger").classList.remove(classes[1], classes[2]);
     state = 0;
-    document.getElementsByTagName("link")[0].remove();
-    document.getElementsByTagName("link")[1].remove();
+    document.getElementsByTagName("link")[2].remove();
     document.getElementsByTagName("link")[2].remove();
   }
 }
